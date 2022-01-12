0c3bf81e446eb458ad730c5195341e4baef687e0
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-12 20:17:37 +0100

    Menu component first code

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({item}) => {
+  const {title, desc, img, price} = item
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <img src={img}/>
+      <p>{price}</p>
+      <p>{desc}</p>
+    </article>
+  );
 };
 
 export default Menu;
