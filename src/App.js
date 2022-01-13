7754cd0fbec731602b718b3b62dfa1d678774f67
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-13 17:21:45 +0100

    changing categories from app components and filter function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Menu from './components/Menu';
 import Categories from './components/Categories';
 import items from './data';
 
 function App() {
   const [food, setFood] = useState(items)
-  const [categories, setCategories] = useState('')
-  console.log(categories)
+  const [categories, setCategories] = useState('lunch')
+
+  const filterMenu = () => {
+    console.log(categories);
+    const filterFood = food.filter( food => food.category === categories)
+    setFood(filterFood);
+  }
+
+  useEffect(() => filterMenu(), [categories])
 
   return (
     <main>
       <Categories callback={setCategories}/>
-      {food.map(food => (
-            <Menu key={food.id} item={food}/>
-          )
+      {
+        food.map(eat => {
+            return <Menu key={eat.id} item={eat}/>
+          }
         )
       }
     </main>
