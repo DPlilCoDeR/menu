55f65bed0d2e08195e025e75aeeb59bae536a056
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-01-13 16:25:49 +0100

    changing categories from Categories component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Categories = ({setCategories}) => {
-  return <section>
-    <button onClick={() => setCategories('breakfast')}>Breakfast</button>
-  </section>;
+const Categories = ({callback}) => {
+  return (
+    <section>
+      <button onClick={() => callback('breakfast')}>Breakfast</button>
+      <button onClick={() => callback('lunch')}>lunch</button>
+      <button onClick={() => callback('shakes')}>shakes</button>
+
+    </section>
+  )
 };
 
 export default Categories;
